33cf13d07abff6b743583f89e9dccfd72643318e
Aby Abraham <abyabrkal@gmail.com>
2019-03-18 11:36:56 +0400

    BulkUpdate to local database server

--- PouchDB/pouch-node/doc.js
+++ PouchDB/pouch-node/doc.js
@@ -2,14 +2,21 @@ const PouchDB = require('pouchdb');
 
 const db = new PouchDB('http://127.0.0.1:5984/people');
 
+/*
 doc = {
     _id: new Date().toISOString(),
     name: 'Peter',
     age: 23,
     occupation: 'designer',
 };
+*/
 
-db.put(doc)
+doc1 = {_id: new Date().toISOString(), name: 'Tony', age: 42, occupation: 'Hero' };
+doc2 = {_id: new Date().toISOString(), name: 'Steve', age: 172, occupation: 'Captain' };
+doc3 = {_id: new Date().toISOString(), name: 'Thor', age: 542, occupation: 'King' };
+doc = [doc1, doc2, doc3];
+
+db.bulkDocs(doc)
     .then(res => {
         console.log('Document inserted OK.');
     }).catch(err => {
